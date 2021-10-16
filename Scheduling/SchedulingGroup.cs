52824d20d3a6c5e0dc19bba5fe830d2f8d8a8103
Steve Cheng <steve.ckp@gmail.com>
2021-10-16 16:37:07 -0400

    Add base class for credit-based job scheduling

---
+++ Scheduling/SchedulingGroup.cs
@@ -0,0 +1,261 @@
+﻿using System;
+
+namespace JobBank.Scheduling
+{
+    public class ScheduledJob
+    {
+        public int EstimatedTime { get; set; }
+    }
+
+    /// <summary>
+    /// Credit-based scheduling from a set of <see cref="SchedulingUnit" />.
+    /// </summary>
+    public class SchedulingGroup
+    {
+        /// <summary>
+        /// Organizes the child queues so that the 
+        /// active child with the highest credit balance
+        /// can be quickly accessed.
+        /// </summary>
+        private IntPriorityHeap<SchedulingUnit> _priorityHeap;
+
+        /// <summary>
+        /// List of all the child queues managed by this instance.
+        /// </summary>
+        /// <remarks>
+        /// In this array, the child queues can be in any order.
+        /// Thus, when deleting a member, the last member 
+        /// can be swapped in to the vacated slot, so that
+        /// all the members occupy consecutive slots starting
+        /// from the beginning of this array.  The slots
+        /// on and after the index <see cref="_numChildren" />
+        /// are set to null; the first such slot can be taken
+        /// when a new member has to be set.
+        /// </remarks>
+        private SchedulingUnit?[] _allChildren;
+
+        /// <summary>
+        /// The number of child queues managed by this instance
+        /// currently.
+        /// </summary>
+        private int _numChildren = 0;
+
+        protected const int MaxCapacity = 1024;
+
+        protected SchedulingGroup(int capacity)
+        {
+            if (capacity < 0 || capacity > MaxCapacity)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            _priorityHeap = new IntPriorityHeap<SchedulingUnit>(
+                (ref SchedulingUnit item, int index) => item.PriorityHeapIndex = index);
+
+            _allChildren = capacity > 0 ? new SchedulingUnit?[capacity] 
+                                        : Array.Empty<SchedulingUnit?>();
+        }
+
+        /// <summary>
+        /// Re-fill balances on all child queues when none are eligible
+        /// to be scheduled.
+        /// </summary>
+        /// <returns>
+        /// Whether there is at least one active child queue
+        /// (after re-filling).
+        /// </returns>
+        private bool RefillBalances()
+        {
+            // Minimum required number of credits to bring
+            // at least one child to non-negative credit.
+            int minimum = int.MaxValue;
+
+            bool hasActiveChild = false;
+
+            for (int i = 0; i < _allChildren.Length; ++i)
+            {
+                var child = _allChildren[i];
+                if (child != null && child.IsActive && child.Balance <= 0)
+                {
+                    hasActiveChild = true;
+                    minimum = Math.Min(minimum, -child.Balance);
+                }
+            }
+
+            if (!hasActiveChild)
+                return false;
+
+            minimum += 1;
+
+            _priorityHeap.Clear();
+
+            for (int i = 0; i < _allChildren.Length; ++i)
+            {
+                var child = _allChildren[i];
+                if (child != null)
+                {
+                    var balance = child.Balance;
+                    balance = Math.Min(balance + minimum, 0) + 9999;
+                    child.Balance = balance;
+
+                    if (child.IsActive && balance > 0)
+                        _priorityHeap.Insert(balance, child);
+                }
+            }
+
+            return true;
+        }
+
+        // De-queue operation.
+        // Take the queue with the most priority.
+        protected ScheduledJob? TakeJob()
+        {
+            ScheduledJob? job = null;
+
+            do
+            {
+                // If no child queues are eligible for de-queuing,
+                // that means they are all inactive or they have
+                // run out of credits.  Try to re-fill their credits.
+                if (_priorityHeap.IsEmpty && !RefillBalances())
+                    break;
+
+                var (balance, child) = _priorityHeap[0];
+                job = child.TakeJob(out int charge);
+
+                if (job == null)
+                    child.IsActive = false;
+
+                balance -= charge;
+                child.Balance = balance;
+
+                if (job != null && balance > 0)
+                    _priorityHeap.ChangeKey(0, balance);
+                else
+                    _priorityHeap.TakeMaximum();
+            } while (job == null);
+
+            return job;
+        }
+
+        /// <summary>
+        /// Add a new (abstract) child queue to this job queue group.
+        /// </summary>
+        /// <param name="jobSource">
+        /// The source of the jobs in the new (abstract) child queue.
+        /// </param>
+        protected SchedulingUnit AddChild(IJobSource jobSource)
+        {
+            var allChildren = _allChildren;
+            int index = _numChildren++;
+            if (index == allChildren.Length)
+            {
+                // Grow capacity of array by 1/2
+                var newArray = new SchedulingUnit?[index + (index >> 1)];
+                Array.Copy(allChildren, newArray, allChildren.Length);
+                _allChildren = allChildren = newArray;
+            }
+
+            var child = new SchedulingUnit(this, jobSource) { Index = index };
+            allChildren[index] = child;
+            return child;
+        }
+
+        /// <summary>
+        /// Remove the child queue that had been added by <see cref="AddChild" />
+        /// earlier.
+        /// </summary>
+        /// <param name="child">
+        /// The child queue to remove, which must have been created by this
+        /// parent.
+        /// </param>
+        private void RemoveChild(SchedulingUnit child)
+        {
+            var index = child.Index;
+            if (index < 0 || child.Parent != this)
+                throw new ArgumentException("Child queue is not present in the parent queue. ", nameof(child));
+
+            var allChildren = _allChildren;
+            child.Index = -1;
+
+            if (child.PriorityHeapIndex >= 0)
+                _priorityHeap.Delete(child.PriorityHeapIndex);
+
+            var lastIndex = --_numChildren;
+            if (lastIndex > 0)
+            {
+                ref SchedulingUnit? lastChild = ref allChildren[lastIndex];
+                allChildren[index] = lastChild;
+                lastChild!.Index = index;
+                lastChild = null;
+            }
+        }
+
+        /// <summary>
+        /// Ensure a child is marked active and put it into
+        /// the priority heap if it has positive balance.
+        /// </summary>
+        /// <param name="child">
+        /// The child scheduling unit.  This instance
+        /// must be its parent.
+        /// </param>
+        internal void ActivateChild(SchedulingUnit child)
+        {
+            if (!child.IsActive)
+            {
+                child.IsActive = true;
+                if (child.Balance > 0)
+                    _priorityHeap.Insert(child.Balance, child);
+            }
+        }
+
+        /// <summary>
+        /// De-activate a child and take it out of the priority
+        /// heap if it is there.
+        /// </summary>
+        /// <param name="child">
+        /// The child scheduling unit.  This instance
+        /// must be its parent.
+        /// </param>
+        internal void DeactivateChild(SchedulingUnit child)
+        {
+            if (child.IsActive)
+            {
+                child.IsActive = false;
+                _priorityHeap.Delete(child.PriorityHeapIndex);
+            }
+        }
+
+        /// <summary>
+        /// Change the debit balance of a child and re-schedule
+        /// it into the priority heap if necessary.
+        /// </summary>
+        /// <param name="child">
+        /// The child scheduling unit.  This instance
+        /// must be its parent.
+        /// </param>
+        /// <param name="debit">
+        /// The amount to add to the child's debit balance.
+        /// </param>
+        internal void AdjustChildBalance(SchedulingUnit child, int debit)
+        {
+            int oldBalance = child.Balance;
+            int newBalance = oldBalance + debit;
+
+            if (child.IsActive)
+            {
+                if (oldBalance > 0)
+                {
+                    if (newBalance <= 0)
+                        _priorityHeap.Delete(child.PriorityHeapIndex);
+                    else
+                        _priorityHeap.ChangeKey(child.PriorityHeapIndex, newBalance);
+                }
+                else if (newBalance > 0)
+                {
+                    _priorityHeap.Insert(newBalance, child);
+                }
+            }
+
+            child.Balance = newBalance;
+        }
+    }
+}
---
+++ Scheduling/SchedulingUnit.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace JobBank.Scheduling
+{
+    /// <summary>
+    /// Represents a generic child queue in a parent queue system
+    /// for fair job scheduling.
+    /// </summary>
+    public sealed class SchedulingUnit
+    {
+        /// <summary>
+        /// The queue group that owns this child queue.
+        /// </summary>
+        public SchedulingGroup Parent { get; }
+
+        /// <summary>
+        /// The index of this child in <see cref="SchedulingGroup._allChildren" />,
+        /// retained so that it can be deleted.
+        /// </summary>
+        /// <remarks>
+        /// This index is -1 if the child is no longer present in the
+        /// parent <see cref="SchedulingGroup" />.
+        /// </remarks>
+        internal int Index { get; set; }
+
+        /// <summary>
+        /// The index of this child queue in the parent's priority heap.
+        /// </summary>
+        /// <remarks>
+        /// The parent will update this index if the child gets re-arranged
+        /// with the other children inside the priority heap.
+        /// </remarks>
+        internal int PriorityHeapIndex { get; set; }
+
+        /// <summary>
+        /// Get or set the current balance of abstract time remaining
+        /// that this child queue is entitled to in fair job scheduling.
+        /// </summary>
+        /// <remarks>
+        /// This balance should be decreased by the charge for each job
+        /// as it is de-queued.  The parent will set this property
+        /// when the balance needs to be re-filled or adjusted.
+        /// </remarks>
+        public int Balance { get; internal set; }
+
+        /// <summary>
+        /// Whether this queue is active, i.e. it may have a job available
+        /// from the next call to <see cref="TakeJob" />.
+        /// </summary>
+        public bool IsActive { get; internal set; }
+
+        /// <summary>
+        /// Prepare a new child queue.
+        /// </summary>
+        /// <param name="jobSource">
+        /// Provides the job items when the abstract child queue is "de-queued".
+        /// </param>
+        internal SchedulingUnit(SchedulingGroup parent, IJobSource jobSource)
+        {
+            Parent = parent;
+            JobSource = jobSource;
+        }
+
+        /// <summary>
+        /// Pulls out a job to process when requested
+        /// by the parent <see cref="SchedulingGroup" />.
+        /// </summary>
+        public IJobSource JobSource { get; }
+
+        /// <summary>
+        /// Calls <see cref="IJobSource.TakeJob" />.
+        /// </summary>
+        /// <param name="charge">The amount of credit to charge
+        /// to this child queue for the new job.
+        /// </param>
+        /// <returns>
+        /// The job that the parent <see cref="SchedulingGroup" />
+        /// should be processing next, or null if there is none
+        /// from this child queue.
+        /// </returns>
+        internal ScheduledJob? TakeJob(out int charge)
+            => JobSource.TakeJob(out charge);
+
+        /// <summary>
+        /// Ensure this scheduling unit is considered active for scheduling
+        /// within its parent group.
+        /// </summary>
+        public void Activate() => Parent.ActivateChild(this);
+
+        /// <summary>
+        /// Exclude this scheduling unit from being actively considered 
+        /// for scheduling within its parent group.
+        /// </summary>
+        public void Deactivate() => Parent.DeactivateChild(this);
+
+        /// <summary>
+        /// Adjust the debit balance of this scheduling unit to affect
+        /// its priority amongst other units in its parent scheduling group.
+        /// </summary>
+        /// <param name="debit">
+        /// The amount to add to <see cref="Balance" />.
+        /// </param>
+        public void AdjustBalance(int debit) => Parent.AdjustChildBalance(this, debit);
+    }
+}
