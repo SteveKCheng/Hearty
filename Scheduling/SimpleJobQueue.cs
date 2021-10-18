bc74860e5ebeb997e979d89fa6c62661fff66bf0
Steve Cheng <steve.ckp@gmail.com>
2021-10-17 22:38:19 -0400

    Add a simple implementation of a job queue as SchedulingUnit

---
+++ Scheduling/SimpleJobQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace JobBank.Scheduling
+{
+    /// <summary>
+    /// A job queue, for the fair scheduling system, backed by 
+    /// <see cref="ChannelReader{T}" />.
+    /// </summary>
+    public class SimpleJobQueue<TJob> : SchedulingUnit<TJob>
+    {
+        private readonly ChannelReader<TJob> _channelReader;
+
+        public SimpleJobQueue(SchedulingGroup<TJob> parent, 
+                              ChannelReader<TJob> channelReader,
+                              int weight = 1)
+            : base(parent, weight)
+        {
+            _channelReader = channelReader;
+        }
+
+        protected override TJob? TakeJob(out int charge)
+        {
+            charge = 0;
+            if (_channelReader.TryRead(out var job))
+                return job;
+
+            _ = ActivateWhenReadyAsync();
+            return default;
+        }
+
+        private async Task ActivateWhenReadyAsync()
+        {
+            bool hasMore = await _channelReader.WaitToReadAsync()
+                                               .ConfigureAwait(false);
+            if (hasMore)
+                Activate();
+        }
+    }
+}
