bef53ea2680d6cab5e544c6320c2e7d94c704c93
Steve Cheng <steve.ckp@gmail.com>
2021-11-06 19:14:03 -0400

    Have SchedulingQueue de-activate itself when empty
    Without this feature, when the parent scheduling group is
    exposed as a channel, it would return (true) from
    ChannelReader.WaitForReadAsync yet ChannelReader.TryRead
    would immediately return false.
    
    In theory that is allowed behavior from a channel, but it
    is unintuitive and inefficient for readers.

--- Scheduling/SchedulingQueue.cs
+++ Scheduling/SchedulingQueue.cs
@@ -30,7 +30,7 @@ namespace JobBank.Scheduling
         private readonly ConcurrentQueue<T> _queue;
 
         /// <inheritdoc cref="IReadOnlyCollection{T}.Count" />
-        public int Count => _queue.Count;
+        public int Count => _count;
 
         /// <inheritdoc cref="ISchedulingFlow{T}.AsFlow" />
         public SchedulingFlow<T> AsFlow() => this;
@@ -43,12 +43,37 @@ namespace JobBank.Scheduling
             _queue = new ConcurrentQueue<T>();
         }
 
+        /// <summary>
+        /// Current count of items, to optimize activation/de-activation of this queue.
+        /// </summary>
+        private int _count = 0;
+
         /// <inheritdoc />
         protected override bool TryTakeItem([MaybeNullWhen(false)] out T item, out int charge)
         {
             bool success = _queue.TryDequeue(out item);
-            charge = success ? item!.InitialCharge : default;
-            return success;
+            if (!success)
+            {
+                charge = default;
+                return false;
+            }
+
+            charge = item!.InitialCharge;
+
+            if (Interlocked.Decrement(ref _count) == 0)
+            {
+                // In the common case of emptying the queue, when there is no race,
+                // de-activate the queue immediately so SchedulingGroup does not
+                // poll it again.
+                Deactivate();
+
+                // If Deactivate above raced with Activate in Enqueue,
+                // then Activate again to be safe.
+                if (_count > 0)
+                    Activate();
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -61,8 +86,15 @@ namespace JobBank.Scheduling
         /// <param name="item">The item to push to the queue. </param>
         public void Enqueue(T item)
         {
+            // Increment first to avoid negative counts when this method
+            // races with TryTakeItem.
+            bool toActivate = (Interlocked.Increment(ref _count) <= 1);
+
             _queue.Enqueue(item);
-            Activate();
+
+            // Activate takes locks, which we can avoid most of the time.
+            if (toActivate)
+                Activate();
         }
 
         /// <inheritdoc cref="IEnumerable{T}.GetEnumerator" />
