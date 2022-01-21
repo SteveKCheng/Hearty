0d89c8884ea88c6a3e55d2a6016227dff2be8531
Steve Cheng <steve.ckp@gmail.com>
2022-01-20 21:11:22 -0500

    Add RemoteWorkerProxy class

---
+++ Server.Core/RemoteWorkerProxy.cs
@@ -0,0 +1,76 @@
+﻿using JobBank.Scheduling;
+using JobBank.WebSockets;
+using JobBank.Work;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JobBank.Server
+{
+    /// <summary>
+    /// Submits promise-backed jobs to a remote worker
+    /// for distributed job scheduling.
+    /// </summary>
+    /// <remarks>
+    /// This class proxies for a remote worker in the distributed job scheduling
+    /// implemented by <see cref="JobSchedulingSystem" />.  The job inputs
+    /// are serialized and sent to the remote host for
+    /// processing, over an asynchronous RPC protocol.
+    /// </remarks>
+    public class RemoteWorkerProxy : IJobWorker<PromiseJob, PromiseData>
+                                   , IJobSubmission
+    {
+        /// <inheritdoc cref="IWorkerNotification.Name" />
+        public string Name { get; }
+
+        /// <inheritdoc cref="IWorkerNotification.IsAlive" />
+        public bool IsAlive => true;
+
+        /// <inheritdoc cref="IWorkerNotification.OnEvent" />
+        public event EventHandler<WorkerEventArgs>? OnEvent;
+
+        void IJobWorker<PromiseJob, PromiseData>.AbandonJob(uint executionId)
+        {
+        }
+
+        async ValueTask<PromiseData>
+            IJobWorker<PromiseJob, PromiseData>.ExecuteJobAsync(
+                uint executionId,
+                IRunningJob<PromiseJob> runningJob,
+                CancellationToken cancellationToken)
+        {
+            var contentType = runningJob.Input.Data.SuggestedContentType;
+
+            var reply = await RunJobAsync(new RunJobRequestMessage
+            {
+                Route = runningJob.Input.Route,
+                ContentType = contentType,
+                Data = await runningJob.Input
+                                       .Data
+                                       .GetPayloadAsync(contentType, cancellationToken)
+                                       .ConfigureAwait(false)
+            }, cancellationToken).ConfigureAwait(false);
+
+            var output = new Payload(reply.ContentType, reply.Data);
+            return output;
+        }
+
+        public RemoteWorkerProxy(string name, RpcConnection rpc)
+        {
+            Name = name;
+            _rpc = rpc;
+        }
+
+        private readonly RpcConnection _rpc;
+
+        /// <inheritdoc cref="IJobSubmission.RunJobAsync" />
+        public ValueTask<RunJobReplyMessage> 
+            RunJobAsync(RunJobRequestMessage request, 
+                        CancellationToken cancellationToken)
+        {
+            return _rpc.InvokeRemotelyAsync<RunJobRequestMessage, 
+                                            RunJobReplyMessage>(
+                WorkerHost.TypeCode_RunJob, request, cancellationToken);
+        }
+    }
+}
