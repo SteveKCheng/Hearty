4fa415b4b2748391f080c88cbc2a5d74225cc446
Steve Cheng <steve.ckp@gmail.com>
2022-02-06 14:37:59 -0500

    Implement feature in SchedulingQueue for "macro jobs"

---
+++ Scheduling/AsyncExpandable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobBank.Scheduling
+{
+    /// <summary>
+    /// The discriminated union of either one value, or a multitude
+    /// of values that are to be asynchronously produced.
+    /// </summary>
+    /// <typeparam name="T">The type of one value.
+    /// </typeparam>
+    /// <remarks>
+    /// This type supports the dynamic expansion of sub-sequences
+    /// of items in <see cref="SchedulingQueue{T}" />.
+    /// </remarks>
+    public readonly struct AsyncExpandable<T>
+    {
+        /// <summary>
+        /// Holds a single value, if <see cref="Multiple" />
+        /// is not null.
+        /// </summary>
+        /// <remarks>
+        /// If <see cref="Multiple" /> is non-null then this
+        /// member is set to the default value for its type.
+        /// </remarks>
+        public readonly T Single;
+
+        /// <summary>
+        /// An enumerator that can produce a sequence of
+        /// values asynchronously, if this instance represents
+        /// such a sequence of values.
+        /// </summary>
+        public readonly IAsyncEnumerable<T>? Multiple;
+
+        /// <summary>
+        /// Wrap a single value.
+        /// </summary>
+        public AsyncExpandable(T single)
+        {
+            Single = single;
+            Multiple = null;
+        }
+
+        /// <summary>
+        /// Wrap a sequence of values.
+        /// </summary>
+        public AsyncExpandable(IAsyncEnumerable<T> multiple)
+        {
+            Single = default!;
+            Multiple = multiple;
+        }
+    }
+}
--- Scheduling/SchedulingQueue.cs
+++ Scheduling/SchedulingQueue.cs
@@ -3,9 +3,8 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-using System.Runtime.Intrinsics.X86;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace JobBank.Scheduling
 {
@@ -25,12 +24,46 @@ namespace JobBank.Scheduling
     /// Use <see cref="SchedulingChannel{T}" /> instead of this class 
     /// to adapt an existing channel as a scheduling flow.
     /// </para>
+    /// <para>
+    /// This scheduling flow has the advanced feature of allowing entries
+    /// of type <see cref="IAsyncEnumerable{T}" /> to be placed in 
+    /// the queue also.  When such an entry is de-queued, they may
+    /// expand asynchronously to a sequence of items of type
+    /// <typeparamref name="T"/>, which are considered
+    /// to cut in front of the queue.  
+    /// </para>
+    /// <para>
+    /// This feature can be used in job scheduling to push in a "macro job" 
+    /// which dynamically expand into hundreds or even thousands of "micro jobs", 
+    /// without having to materialize all of them up front to put into the queue.
+    /// Certain applications may be able to represent the "micro jobs" a 
+    /// more efficient manner than a straight in-memory sequence of 
+    /// <typeparamref name="T" />.
+    /// </para>
+    /// <para>
+    /// This transformation must be implemented at the level of a scheduling 
+    /// flow, and not on top of the scheduling group, because the items 
+    /// in the sub-sequence need to stay in the same scheduling flow
+    /// to be prioritized correctly.
+    /// </para>
     /// </remarks>
     public sealed class SchedulingQueue<T> 
-        : SchedulingFlow<T>, ISchedulingFlow<T>, ISchedulingAccount, IReadOnlyCollection<T>
+        : SchedulingFlow<T>
+        , ISchedulingFlow<T>
+        , ISchedulingAccount
+        , IReadOnlyCollection<AsyncExpandable<T>>
         where T: ISchedulingExpense
     {
-        private readonly ConcurrentQueue<T> _queue;
+        /// <summary>
+        /// Holds items and sub-sequences that are dequeued
+        /// from the front and enqueued at the back.
+        /// </summary>
+        private readonly ConcurrentQueue<AsyncExpandable<T>> _queue;
+
+        /// <summary>
+        /// Current count of items, to optimize activation/de-activation of this queue.
+        /// </summary>
+        private int _count = 0;
 
         /// <inheritdoc cref="IReadOnlyCollection{T}.Count" />
         public int Count => _count;
@@ -43,26 +76,184 @@ namespace JobBank.Scheduling
         /// </summary>
         public SchedulingQueue()
         {
-            _queue = new ConcurrentQueue<T>();
+            _queue = new ConcurrentQueue<AsyncExpandable<T>>();
+        }
+
+        #region Expanding entries that are sub-sequences
+
+        /// <summary>
+        /// The enumerator, if any, that should be consulted first
+        /// before any items in <see cref="_queue" />.
+        /// </summary>
+        private IAsyncEnumerator<T>? _currentEnumerator;
+
+        /// <summary>
+        /// The saved return value of <see cref="IAsyncEnumerator{T}.MoveNextAsync" />,
+        /// for retrieving the next item from <see cref="_currentEnumerator" />.
+        /// </summary>
+        private ValueTask<bool> _currentEnumeratorTask;
+
+        /// <summary>
+        /// Set to true if <see cref="_currentEnumeratorTask" /> is being awaited,
+        /// to prevent multiple awaiters.
+        /// </summary>
+        private bool _isWaitingOnEnumerator;
+
+        /// <summary>
+        /// Object that is to be locked to defend against improper concurrent
+        /// calls to <see cref="TryTakeItem" />.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="TryTakeItem" /> may manipulate the variables relating
+        /// to the enumerators, which do not tolerate races. 
+        /// That method should not ever be called concurrently
+        /// by <see cref="SchedulingGroup{T}" />, but nevertheless
+        /// we make the implementation robust.  This critical section
+        /// is taken when reading from the scheduling flow only.
+        /// </remarks>
+        private object ReadSideCriticalSection => _queue;
+
+        /// <summary>
+        /// Check for a next item that may be available from the 
+        /// current enumerator, which is always 
+        /// considered to "cut in of the queue".
+        /// </summary>
+        /// <param name="item">
+        /// If there is a next item from the enumerator, it
+        /// will be set here on return.
+        /// </param>
+        /// <param name="charge">
+        /// The initial charge for the item.
+        /// </param>
+        /// <param name="waiting">
+        /// If true, this scheduling flow needs to asynchronously
+        /// wait for the enumerator to produce the next item.
+        /// If false, either there is a next item immediately
+        /// available from the enumerator or the enumerator 
+        /// has been exhausted.
+        /// </param>
+        /// <returns>
+        /// Whether there is a next item immediately available
+        /// from the enumerator.
+        /// </returns>
+        /// <remarks>
+        /// When <paramref name="waiting" /> is false on return,
+        /// this scheduling flow is de-activated, until 
+        /// <see cref="IAsyncEnumerator{T}.MoveNextAsync" /> completes
+        /// asynchronously when the flow is activated again.
+        /// </remarks>
+        private bool TryTakeItemFromEnumerator(
+            [MaybeNullWhen(false)] out T item, 
+            out int charge,
+            out bool waiting)
+        {
+            item = default;
+            charge = default;
+            waiting = false;
+
+            var enumerator = _currentEnumerator;
+            if (enumerator is null)
+                return false;
+
+            var task = _currentEnumeratorTask;
+            if (task.IsCompleted)
+            {
+                _isWaitingOnEnumerator = false;
+
+                if (task.IsCompletedSuccessfully && task.Result == true)
+                {
+                    item = enumerator.Current;
+                    charge = item.InitialCharge;
+                    _currentEnumeratorTask = SafelyAdvanceAsyncEnumerator(enumerator);
+                    return true;
+                }
+                else
+                {
+                    _currentEnumerator = null;
+                    _currentEnumeratorTask = default;
+                    _ = FireAndForgetDisposeAsync(enumerator);
+                    DeactivateOnEmptyQueue();
+                }
+            }
+            else
+            {
+                waiting = true;
+                Deactivate();
+                if (!_isWaitingOnEnumerator)
+                {
+                    _isWaitingOnEnumerator = true;
+                    _ = ActivateOnReadyEnumeratorAsync();
+                }
+            }
+
+            return false;
         }
 
         /// <summary>
-        /// Current count of items, to optimize activation/de-activation of this queue.
+        /// Run <see cref="IAsyncDisposable.DisposeAsync" /> in the background
+        /// and ignore any exceptions.
         /// </summary>
-        private int _count = 0;
-
-        /// <inheritdoc />
-        protected override bool TryTakeItem([MaybeNullWhen(false)] out T item, out int charge)
+        private async Task FireAndForgetDisposeAsync(IAsyncDisposable target)
         {
-            bool success = _queue.TryDequeue(out item);
-            if (!success)
+            try
             {
-                charge = default;
-                return false;
+                await target.DisposeAsync().ConfigureAwait(false);
+            }
+            catch
+            {
+            }
+        }
+
+        /// <summary>
+        /// Continuation on <see cref="IAsyncEnumerator{T}.MoveNextAsync" />
+        /// that asynchronously completes to re-activate this scheduling flow.
+        /// </summary>
+        private async Task ActivateOnReadyEnumeratorAsync()
+        {
+            try
+            {
+                await _currentEnumeratorTask.ConfigureAwait(false);
+            }
+            catch 
+            {
             }
 
-            charge = item!.InitialCharge;
+            Activate();
+        }
 
+        /// <summary>
+        /// Invoke <see cref="IAsyncEnumerator{T}.MoveNextAsync" />,
+        /// treating any thrown exception as if it returned false.
+        /// </summary>
+        private static ValueTask<bool> SafelyAdvanceAsyncEnumerator(IAsyncEnumerator<T> enumerator)
+        {
+            try
+            {
+                return enumerator.MoveNextAsync();
+            }
+            catch
+            {
+                return ValueTask.FromResult(false);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Decrement <see cref="_count" /> after an entry has been successfully
+        /// processed out of it, de-activating the queue if there are no
+        /// more entries.
+        /// </summary>
+        /// <remarks>
+        /// When an entry expands to multiple items via <see cref="IAsyncEnumerator{T}" />,
+        /// the "master" entry is not considered processed,
+        /// and thus <see cref="_count" /> is not decremented, 
+        /// until the last of the expanded items has been seen. 
+        /// So enqueuing new entries (from <see cref="Enqueue(T)" />) 
+        /// while an enumerator is active do not re-activate the scheduling flow.
+        /// </remarks>
+        private void DeactivateOnEmptyQueue()
+        {
             if (Interlocked.Decrement(ref _count) == 0)
             {
                 // In the common case of emptying the queue, when there is no race,
@@ -75,33 +266,94 @@ namespace JobBank.Scheduling
                 if (_count > 0)
                     Activate();
             }
+        }
 
-            return true;
+        /// <inheritdoc />
+        protected override bool TryTakeItem([MaybeNullWhen(false)] out T item, out int charge)
+        {
+            lock (ReadSideCriticalSection)
+            {
+                AsyncExpandable<T> entry;
+
+                while (true)
+                {
+                    if (TryTakeItemFromEnumerator(out item, out charge, out bool waiting))
+                        return true;
+
+                    if (waiting)
+                        return false;
+
+                    if (!_queue.TryDequeue(out entry))
+                    {
+                        item = default;
+                        charge = default;
+                        return false;
+                    }
+
+                    var enumerable = entry.Multiple;
+                    if (enumerable is null)
+                        break;
+
+                    var enumerator = enumerable.GetAsyncEnumerator();
+                    _currentEnumerator = enumerator;
+                    _currentEnumeratorTask = SafelyAdvanceAsyncEnumerator(enumerator);
+                }
+
+                item = entry.Single;
+                charge = item.InitialCharge;
+
+                DeactivateOnEmptyQueue();
+
+                return true;
+            }
         }
 
         /// <summary>
-        /// Push an item to the end of the queue.
+        /// Push an entry (which may in turn expand to a sub-sequence) 
+        /// to the end of the queue.
         /// </summary>
         /// <remarks>
         /// The queue if activated as a scheduling flow 
         /// if it is not already activated.
         /// </remarks>
-        /// <param name="item">The item to push to the queue. </param>
-        public void Enqueue(T item)
+        /// <param name="entry">The entry to push to the queue. </param>
+        public void Enqueue(AsyncExpandable<T> entry)
         {
             // Increment first to avoid negative counts when this method
             // races with TryTakeItem.
             bool toActivate = (Interlocked.Increment(ref _count) <= 1);
 
-            _queue.Enqueue(item);
+            _queue.Enqueue(entry);
 
             // Activate takes locks, which we can avoid most of the time.
             if (toActivate)
                 Activate();
         }
 
+        /// <summary>
+        /// Push one item to the end of the queue.
+        /// </summary>
+        /// <remarks>
+        /// The queue if activated as a scheduling flow 
+        /// if it is not already activated.
+        /// </remarks>
+        /// <param name="item">The item to push to the queue. </param>
+        public void Enqueue(T item) 
+            => Enqueue(new AsyncExpandable<T>(item));
+
+        /// <summary>
+        /// Push a sub-sequence of items to the end of the queue.
+        /// </summary>
+        /// <remarks>
+        /// The queue if activated as a scheduling flow 
+        /// if it is not already activated.
+        /// </remarks>
+        /// <param name="sequence">The sub-sequence to push to the queue. </param>
+        public void Enqueue(IAsyncEnumerable<T> sequence) 
+            => Enqueue(new AsyncExpandable<T>(sequence));
+
         /// <inheritdoc cref="IEnumerable{T}.GetEnumerator" />
-        public IEnumerator<T> GetEnumerator() => _queue.GetEnumerator();
+        public IEnumerator<AsyncExpandable<T>> GetEnumerator() => _queue.GetEnumerator();
 
         /// <inheritdoc cref="IEnumerable.GetEnumerator" />
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
