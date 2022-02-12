4fa8d95a66141a945727d59803cd889b60a0a119
Steve Cheng <steve.ckp@gmail.com>
2022-02-12 10:47:23 -0500

    Unit test of content negotiation

---
+++ Tests/ContentNegotiationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+using JobBank.Server;
+
+namespace JobBank.Tests
+{
+    public class ContentNegotiationTests
+    {
+        [Fact]
+        public void SelectBestFormat()
+        {
+            var available = new ContentFormatInfo[]
+            {
+                new("application/messagepack", ContentPreference.Best),
+                new("application/json", ContentPreference.Good),
+                new("application/xml", ContentPreference.Bad)
+            };
+
+            var requests = new StringValues(new string[]
+            {
+                // Deliberate use some strange syntax here to check that
+                // parsing is RFC-correct
+                @"text/plain, application/messagepack; dummy=""5\"","", application/json ,",
+                "application/xml",
+                ", multipart/mixed, application/*"
+            });
+
+            int best = ContentFormatInfo.Negotiate(available, requests);
+            Assert.Equal(1, best);
+        }
+    }
+}
