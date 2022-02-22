18065b6f83bce0d872aaa8e3bd3cbba00576bc0a
Steve Cheng <steve.ckp@gmail.com>
2022-02-22 14:04:10 -0500

    Introduce JobQueueSystem and IJobQueueSystem

---
+++ Server.Core/IJobQueueSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBank.Server;
+
+/// <summary>
+/// A unique identifier for a job queue 
+/// <see cref="IJobQueueSystem" />.
+/// </summary>
+/// <param name="Owner">Representation of the owner of the queue. </param>
+/// <param name="Priority">
+/// The priority class, numbered from 0 to 
+/// <see cref="IJobQueueSystem.PriorityClassesCount" /> minus one.
+/// </param>
+/// <param name="Name">
+/// The name of the queue within the set of queues with the
+/// same owner and priority class.
+/// </param>
+public record struct JobQueueKey(IJobQueueOwner Owner, 
+                                 int Priority,
+                                 string Name);
+
+/// <summary>
+/// Provides access to the job queues for all clients.
+/// </summary>
+public interface IJobQueueSystem
+{
+    /// <summary>
+    /// Get the queue to push jobs into for a given client,
+    /// creating it if it does not exist.
+    /// </summary>
+    /// <param name="key">Identifier assigned to the job queue. 
+    /// </param>
+    /// <returns>
+    /// The desired queue, which may be newly created.
+    /// </returns>
+    ClientJobQueue GetOrAddJobQueue(JobQueueKey key);
+
+    /// <summary>
+    /// Get the queue to push jobs into for a given client,
+    /// if it exists.
+    /// </summary>
+    /// <param name="key">Identifier assigned to the job queue. 
+    /// </param>
+    /// <returns>
+    /// The desired queue, or null if it has not been created
+    /// before or has expired.
+    /// </returns>
+    ClientJobQueue? TryGetJobQueue(JobQueueKey key);
+
+    /// <summary>
+    /// The number of priority classes available.
+    /// </summary>
+    int PriorityClassesCount { get; }
+}
---
+++ Server.Core/JobQueueSystem.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using JobBank.Scheduling;
+
+namespace JobBank.Server;
+
+using JobMessage = ILaunchableJob<PromisedWork, PromiseData>;
+
+/// <summary>
+/// Manages a system of queues for scheduling clients' jobs.
+/// </summary>
+public sealed class JobQueueSystem : IJobQueueSystem, IAsyncDisposable, IDisposable
+{
+    private readonly PrioritizedQueueSystem<
+                        JobMessage,
+                        ClientQueueSystem<
+                            JobMessage,
+                            IJobQueueOwner,
+                            ClientQueueSystem<
+                                JobMessage,
+                                string,
+                                ClientJobQueue>>> _priorityClasses;
+
+    private readonly SimpleExpiryQueue _expiryQueue;
+
+    /// <inheritdoc cref="IJobQueueSystem.PriorityClassesCount" />
+    public int PriorityClassesCount => _priorityClasses.Count;
+
+    private ClientQueueSystem<JobMessage,
+                              string,
+                              ClientJobQueue> 
+        CreateInnerQueueSystem()
+    {
+        return new(factory: key => new ClientJobQueue(),
+                   _expiryQueue);
+    }
+
+    private IEnumerable<ClientQueueSystem<
+                            JobMessage, 
+                            IJobQueueOwner,
+                            ClientQueueSystem<
+                                JobMessage,
+                                string,
+                                ClientJobQueue>>>
+        GenerateMiddleQueueSystems(int count)
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            yield return new(factory: key => CreateInnerQueueSystem(),
+                             _expiryQueue);
+        }
+    }
+
+    /// <inheritdoc cref="IJobQueueSystem.GetOrAddJobQueue" />
+    public ClientJobQueue GetOrAddJobQueue(JobQueueKey key)
+    {
+        return _priorityClasses[key.Priority].GetOrAdd(key.Owner).GetOrAdd(key.Name);
+    }
+
+    /// <inheritdoc cref="IJobQueueSystem.TryGetJobQueue" />
+    public ClientJobQueue? TryGetJobQueue(JobQueueKey key)
+    {
+        var priorityClass = _priorityClasses[key.Priority];
+        if (!priorityClass.TryGetValue(key.Owner, out var innerQueueSystem))
+            return null;
+
+        if (!innerQueueSystem.TryGetValue(key.Name, out var clientJobQueue))
+            return null;
+
+        return clientJobQueue;
+    }
+
+    /// <summary>
+    /// Asynchronously stop dispatching clients' jobs to workers.
+    /// </summary>
+    public ValueTask DisposeAsync()
+    {
+        _cancelSource.Cancel();
+        return new ValueTask(_jobRunnerTask);
+    }
+
+    /// <summary>
+    /// Stop dispatching clients' jobs to workers, 
+    /// synchronously waiting until the internal dispatching task completes.
+    /// </summary>
+    public void Dispose() => DisposeAsync().AsTask().Wait();
+
+    /// <summary>
+    /// Construct with a fixed number of priority classes.
+    /// </summary>
+    /// <param name="countPriorities">
+    /// The desired number of priority classes.
+    /// </param>
+    public JobQueueSystem(int countPriorities,
+                          WorkerDistribution<PromisedWork, PromiseData> workerDistribution)
+    {
+        _expiryQueue = new SimpleExpiryQueue(60000, 20);
+        _priorityClasses = new(GenerateMiddleQueueSystems(countPriorities));
+
+        _cancelSource = new CancellationTokenSource();
+
+        _jobRunnerTask = JobScheduling.RunJobsAsync(
+                            _priorityClasses.AsChannel(),
+                            workerDistribution.AsChannel(),
+                            _cancelSource.Token);
+    }
+
+    /// <summary>
+    /// Used to stop <see cref="_jobRunnerTask" />.
+    /// </summary>
+    private readonly CancellationTokenSource _cancelSource;
+
+    /// <summary>
+    /// Task that de-queues jobs and dispatches them to workers.
+    /// </summary>
+    private readonly Task _jobRunnerTask;
+}
