a142a48db38f47bad3d4e843d320751a97cc8023
Steve Cheng <steve.ckp@gmail.com>
2022-03-09 12:47:55 -0500

    Move MockJobLoad page out of WebUi library since it depends on mocks

--- Server.Demo/Pages/MockJobLoad.razor.cs
+++ Server.Demo/Pages/MockJobLoad.razor.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Hearty.Server.WebUi.Pages
+namespace Hearty.Server.Demo.Pages
 {
     public partial class MockJobLoad
     {
