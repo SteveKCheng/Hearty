638f50694b9271e076a040234fc7281e7cf97753
Steve Cheng <steve.ckp@gmail.com>
2022-03-14 16:39:27 -0400

    Add simple list display of current jobs in queue dashboard

--- Server.WebUi/Pages/PriorityClasses.razor.cs
+++ Server.WebUi/Pages/PriorityClasses.razor.cs
@@ -1,40 +1,55 @@
-﻿using System;
+﻿using Hearty.Common;
+using System;
+using System.Collections.Generic;
 
-namespace Hearty.Server.WebUi.Pages
+namespace Hearty.Server.WebUi.Pages;
+
+public partial class PriorityClasses : IDisposable
 {
-    public partial class PriorityClasses : IDisposable
+    private bool _isDisposed;
+
+    protected override void OnInitialized()
     {
-        private bool _isDisposed;
+        _timeoutProvider.Register(TimeoutBucket.After1Second,
+            (_, _) =>
+            {
+                // TimeoutProvider does not provide cancellation, so
+                // keep polling until the page is disposed.  False
+                // positives can occur when this check races with
+                // disposal, but they are harmless.
+                if (_isDisposed)
+                    return false;
 
-        protected override void OnInitialized()
-        {
-            _timeoutProvider.Register(TimeoutBucket.After1Second,
-                (_, _) =>
+                InvokeAsync(() =>
                 {
-                    // TimeoutProvider does not provide cancellation, so
-                    // keep polling until the page is disposed.  False
-                    // positives can occur when this check races with
-                    // disposal, but they are harmless.
-                    if (_isDisposed)
-                        return false;
+                    // Definitely check this flag again after we
+                    // enter the page's synchronization context to
+                    // avoid races, assuming disposal also happens
+                    // in the same synchronization context.
+                    if (!_isDisposed)
+                        StateHasChanged();
+                });
 
-                    InvokeAsync(() =>
-                    {
-                        // Definitely check this flag again after we
-                        // enter the page's synchronization context to
-                        // avoid races, assuming disposal also happens
-                        // in the same synchronization context.
-                        if (!_isDisposed)
-                            StateHasChanged();
-                    });
+                return true;
+            }, null);
+    }
 
-                    return true;
-                }, null);
-        }
+    private HashSet<JobQueueKey>? _queuesToExpand;
 
-        public void Dispose()
-        {
-            _isDisposed = true;
-        }
+    private bool ShouldDisplayInDetail(in JobQueueKey targetKey)
+    {
+        return _queuesToExpand?.Contains(targetKey) == true;
+    }
+
+    private void AddToDetailDisplay(in JobQueueKey targetKey)
+    {
+        var q = _queuesToExpand ??= new HashSet<JobQueueKey>();
+        q.Add(targetKey);
+    }
+
+    /// <inheritdoc cref="IDisposable.Dispose" />
+    public void Dispose()
+    {
+        _isDisposed = true;
     }
 }
