430873ef18573086951d0a78dfe5b6fa95bc98e3
Steve Cheng <steve.ckp@gmail.com>
2022-03-24 12:58:21 -0400

    Add basic test of FasterDbDictionary from Server.FasterKV

---
+++ Tests/FasterKVTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Hearty.Server.FasterKV;
+using System.IO;
+
+namespace Hearty.Tests;
+
+public class FasterKVTests
+{
+    [Fact]
+    public void Basic1()
+    {
+        uint serviceId = 0;
+        uint seq = 1;
+
+        var random = new Random(Seed: 89);
+
+        using var db = new FasterDbDictionary<PromiseId, double>(new FasterDbFileOptions
+        {
+            Path = String.Empty,
+            Preallocate = false,
+            DeleteOnDispose = true
+        });
+
+        var key = new PromiseId(serviceId, seq++);
+        var value = random.NextDouble();
+        db.Add(key, value);
+
+        Assert.True(db.ContainsKey(key));
+        Assert.True(db.TryGetValue(key, out var retrievedValue));
+        Assert.Equal(value, retrievedValue);
+    }
+}
