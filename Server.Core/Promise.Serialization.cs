67e45a99083830467c10a7365dd1ff3b3f3c3c31
Steve Cheng <steve.ckp@gmail.com>
2022-03-26 11:51:03 -0400

    Introduce framework for serializing Promise and PromiseData

---
+++ Server.Core/Promise.Serialization.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Hearty.Server;
+
+public partial class Promise
+{
+    /// <summary>
+    /// Get the header to write when this promise is to be serialized.
+    /// </summary>
+    /// <remarks>
+    /// The header provides the length of the serialized representation
+    /// which is often required for pre-allocating buffers.
+    /// </remarks>
+    /// <returns>
+    /// The header information for the current state of this promise.
+    /// </returns>
+    public PromiseSerializationHeader GetSerializationHeader()
+    {
+        var input = RequestOutput;
+        var output = ResultOutput;
+
+        var inputSerializationInfo = input?.GetSerializationInfo();
+        var outputSerializationInfo = output?.GetSerializationInfo();
+
+        return new PromiseSerializationHeader
+        {
+            HeaderLength = PromiseSerializationHeader.Size,
+            InputSchemaCode = inputSerializationInfo?.SchemaCode ?? 0,
+            OutputSchemaCode = outputSerializationInfo?.SchemaCode ?? 0,
+            Reserved = 0,
+            InputLength = inputSerializationInfo?.PayloadLength ?? 0,
+            OutputLength = outputSerializationInfo?.PayloadLength ?? 0,
+            Id = Id
+        };
+    }
+
+    /// <summary>
+    /// Serialize this promise into buffers.
+    /// </summary>
+    /// <remarks>
+    /// This method is used to persist the promise outside
+    /// of .NET's GC-managed memory.
+    /// </remarks>
+    /// <param name="header">
+    /// The header obtained from <see cref="GetSerializationHeader" />.
+    /// This argument is needed because the current object may see
+    /// (concurrent) updates, in particular, completion of data, which 
+    /// may affect the length of its serialization.  The header is re-read
+    /// to cap the length of the serialization to when the "snapshot"
+    /// of this object was taken.
+    /// </param>
+    /// <param name="dest">
+    /// The buffer to write to.  This method shall write
+    /// exactly the number of bytes implied by <paramref name="header" />.
+    /// </param>
+    public void Serialize(in PromiseSerializationHeader header, IBufferWriter<byte> dest)
+    {
+        var headerLength = (int)header.HeaderLength;
+        header.WriteTo(dest.GetSpan(headerLength));
+        dest.Advance(headerLength);
+
+        if (header.InputLength != 0)
+            RequestOutput!.Serialize(dest);
+
+        if (header.OutputLength != 0)
+            ResultOutput!.Serialize(dest);
+    }
+
+    /// <summary>
+    /// Re-materialize a promise object from what <see cref="Serialize" />
+    /// has written.
+    /// </summary>
+    /// <param name="schemas">
+    /// Mapping of schema codes required to instantiate the correct
+    /// derived classes of <see cref="PromiseData" />.
+    /// </param>
+    /// <param name="data">
+    /// The serialized bytes of the promise.
+    /// </param>
+    /// <returns>
+    /// The de-serialized promise object.
+    /// </returns>
+    public static Promise Deserialize(PromiseDataSchemas schemas,
+                                      ReadOnlyMemory<byte> data)
+    {
+        var header = PromiseSerializationHeader.ReadFrom(data.Span);
+        data = data[header.HeaderLength..];
+
+        PromiseData? inputData = null;
+        PromiseData? outputData = null;
+
+        if (header.InputSchemaCode != 0)
+        {
+            var info = new PromiseDataSerializationInfo
+            {
+                SchemaCode = header.InputSchemaCode,
+                PayloadLength = header.InputLength
+            };
+
+            var reader = PipeReader.Create(new ReadOnlySequence<byte>(data));
+            inputData = schemas[info.SchemaCode].Invoke(info, reader);
+        }
+
+        if (header.OutputSchemaCode != 0)
+        {
+            var info = new PromiseDataSerializationInfo
+            {
+                SchemaCode = header.OutputSchemaCode,
+                PayloadLength = header.OutputLength
+            };
+
+            var reader = PipeReader.Create(new ReadOnlySequence<byte>(data));
+            outputData = schemas[info.SchemaCode].Invoke(info, reader);
+        }
+
+        return new Promise(DateTime.UtcNow, header.Id, inputData, outputData);
+    }
+}
+
+/// <summary>
+/// The header fields for the serialized representation of a promise.
+/// </summary>
+/// <remarks>
+/// <para>
+/// As serialization of promises are used to store them in
+/// in-process databases, serialization should be compact
+/// and quick to read and write.
+/// </para>
+/// <para>
+/// To that end, header fields are stored at fixed offsets,
+/// and this structure can "just" be read off a block of bytes.
+/// Fortunately, the header fields do not need to be variable-length.
+/// </para>
+/// <para>
+/// We ignore the aspect of endian conversion as Hearty, so far,
+/// is expected only to run on little-endian platforms.
+/// </para>
+/// <para>
+/// Clients of this library should not rely on these implementation 
+/// details: for this reason, the fields in this structure are not
+/// public.
+/// </para>
+/// </remarks>
+[StructLayout(LayoutKind.Sequential)]
+public struct PromiseSerializationHeader
+{
+    /// <summary>
+    /// The total length, in bytes, of the serialization of the promise.
+    /// </summary>
+    public long TotalLength => (long)HeaderLength + InputLength + OutputLength;
+
+    /// <summary>
+    /// The length in bytes of this header.  
+    /// </summary>
+    /// <remarks>
+    /// This field serves as a version identifier as well,
+    /// for old data.  New fields added at the end will necessarily
+    /// increase the length of the header.
+    /// </remarks>
+    internal ushort HeaderLength;
+
+    internal ushort InputSchemaCode;
+
+    internal ushort OutputSchemaCode;
+
+    internal ushort Reserved;
+
+    /// <summary>
+    /// The ID of the promise being stored.
+    /// </summary>
+    internal PromiseId Id;
+
+    /// <summary>
+    /// The length in bytes of the serialization of the
+    /// input <see cref="PromiseData" />.  
+    /// </summary>
+    /// <remarks>
+    /// This field is zero if no completed input has been stored
+    /// in the <see cref="Promise" /> object.
+    /// </remarks>
+    internal uint InputLength;
+
+    /// <summary>
+    /// The length in bytes of the serialization of the
+    /// output <see cref="PromiseData" />.  
+    /// </summary>
+    /// <remarks>
+    /// This field is zero if no completed output has been stored
+    /// in the <see cref="Promise" /> object.
+    /// </remarks>
+    internal uint OutputLength;
+
+    internal ulong CreationTime;
+
+    internal ulong CompletionTime;
+
+    /// <summary>
+    /// Read an instance of this structure from a buffer of bytes.
+    /// </summary>
+    internal static PromiseSerializationHeader ReadFrom(ReadOnlySpan<byte> buffer)
+    {
+        if (!MemoryMarshal.TryRead(buffer, out PromiseSerializationHeader header))
+            ThrowExceptionForTooSmallBuffer();
+        return header;
+    }
+
+    /// <summary>
+    /// The size of this structure in bytes.
+    /// </summary>
+    internal static ushort Size => (ushort)Unsafe.SizeOf<PromiseSerializationHeader>();
+
+    /// <summary>
+    /// Write this instance into a buffer of bytes so that it
+    /// can be read again by <see cref="ReadFrom" />.
+    /// </summary>
+    internal readonly void WriteTo(Span<byte> buffer)
+    {
+        if (!MemoryMarshal.TryWrite(buffer, ref Unsafe.AsRef(this)))
+            ThrowExceptionForTooSmallBuffer();
+    }
+
+    private static void ThrowExceptionForTooSmallBuffer()
+    {
+        throw new ArgumentException(
+                message: "The supplied buffer is too small " +
+                         "for PromiseSerializationHeader. ",
+                paramName: "buffer");
+    }
+}
---
+++ Server.Core/PromiseDataSchemas.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Pipelines;
+
+namespace Hearty.Server;
+
+/// <summary>
+/// A mapping of schema codes for <see cref="PromiseData" /> to
+/// their de-serializers.
+/// </summary>
+/// <remarks>
+/// This class is a registry of schema codes to their de-serializers.
+/// It is a dedicated type so that it can be easily put into a dependency
+/// injection framework, and it enforces that the mapping cannot
+/// (concurrently) change.
+/// </remarks>
+public sealed class PromiseDataSchemas : IReadOnlyDictionary<ushort, PromiseDataDeserializer>
+{
+    private readonly ImmutableDictionary<ushort, PromiseDataDeserializer> _entries;
+
+    /// <summary>
+    /// Construct the mapping of schema codes in one shot.
+    /// </summary>
+    /// <param name="entries">
+    /// Listing of schema codes with their associated de-serializers.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    /// <paramref name="entries"/> contains duplicate keys.
+    /// </exception>
+    public PromiseDataSchemas(IEnumerable<KeyValuePair<ushort, PromiseDataDeserializer>> entries)
+        => _entries = entries.ToImmutableDictionary();
+
+    /// <summary>
+    /// Construct from a mapping of schema codes that has been incrementally built.
+    /// </summary>
+    /// <param name="builder">
+    /// Mapping of schema codes to their associated de-serializers.
+    /// </param>
+    public PromiseDataSchemas(ImmutableDictionary<ushort, PromiseDataDeserializer>.Builder builder)
+    {
+        if (!object.ReferenceEquals(builder.KeyComparer, EqualityComparer<ushort>.Default))
+            throw new ArgumentException("Comparer of keys for PromiseDataSchemas must be the default one, but is not. ");
+
+        _entries = builder.ToImmutable();
+    }
+
+    /// <summary>
+    /// Get the de-serializer corresponding to a schema code.
+    /// </summary>
+    /// <param name="key">The schema code. </param>
+    /// <returns>The de-serializer function. </returns>
+    public PromiseDataDeserializer this[ushort key] => _entries[key];
+
+    /// <inheritdoc cref="IReadOnlyDictionary{TKey, TValue}.Keys" />
+    public IEnumerable<ushort> Keys => _entries.Keys;
+
+    /// <inheritdoc cref="IReadOnlyDictionary{TKey, TValue}.Values" />
+    public IEnumerable<PromiseDataDeserializer> Values => _entries.Values;
+
+    /// <inheritdoc cref="IReadOnlyCollection{T}.Count" />
+    public int Count => _entries.Count;
+
+    /// <inheritdoc cref="IReadOnlyDictionary{TKey, TValue}.ContainsKey" />
+    public bool ContainsKey(ushort key) => _entries.ContainsKey(key);
+
+    /// <inheritdoc cref="IEnumerable{T}.GetEnumerator" />
+    public IEnumerator<KeyValuePair<ushort, PromiseDataDeserializer>> GetEnumerator()
+        => _entries.GetEnumerator();
+
+    /// <inheritdoc cref="IReadOnlyDictionary{TKey, TValue}.TryGetValue" />
+    public bool TryGetValue(ushort key, [MaybeNullWhen(false)] out PromiseDataDeserializer value)
+        => _entries.TryGetValue(key, out value);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+/// <summary>
+/// Materializes promise data from a sequence of bytes, i.e. de-serializes.
+/// </summary>
+/// <param name="info">
+/// Basic information about the payload as reported by 
+/// <see cref="PromiseData.GetSerializationInfo" />.
+/// </param>
+/// <param name="pipeReader">
+/// Where the payload for the serialized promise data should be read from.
+/// On success, this function should read exactly the number of bytes
+/// reported by <see cref="PromiseDataSerializationInfo.PayloadLength" />.
+/// </param>
+/// <returns>
+/// The re-materialized instance of <see cref="PromiseData" />.
+/// </returns>
+public delegate PromiseData PromiseDataDeserializer(in PromiseDataSerializationInfo info,
+                                                    PipeReader pipeReader);
+
+
+/// <summary>
+/// Basic information about an instance of <see cref="PromiseData" /> 
+/// to start decoding its serialization.
+/// </summary>
+public readonly struct PromiseDataSerializationInfo
+{
+    /// <summary>
+    /// The length of the payload, in bytes, that the derived class of
+    /// <see cref="PromiseData" /> serializes.
+    /// </summary>
+    /// <remarks>
+    /// This length must be known upfront.  It is populated
+    /// into the header for the serialization of the 
+    /// containing promise, and may be consulted to pre-allocate
+    /// buffers.
+    /// </remarks>
+    public uint PayloadLength { get; init; }
+
+    /// <summary>
+    /// An internal code indicating how to de-serialize the payload.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// These codes are registered into <see cref="PromiseDataSchemas" />
+    /// so that the correct derived class of <see cref="PromiseData" /> 
+    /// to instantiate can be looked up.
+    /// </para>
+    /// <para>
+    /// These codes are private to the job server, and are not exposed
+    /// to clients.  Also, any instance of job server is going to
+    /// use only a handful of derived classes of <see cref="PromiseData" />.  
+    /// So, a 16-bit integer suffices to encompass all reasonable codes,
+    /// and reduces size and complexity (especially compared to 
+    /// arbitrary strings).
+    /// </para>
+    /// </remarks>
+    public ushort SchemaCode { get; init; }
+}
