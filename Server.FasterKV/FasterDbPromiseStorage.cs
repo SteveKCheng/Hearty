a3b801ef4a9571514a133f6b711b8bf9e0ff6a07
Steve Cheng <steve.ckp@gmail.com>
2022-03-27 14:23:36 -0400

    Sketch of on-demand de-serialization of Promise from FASTER KV database

--- Server.FasterKV/FasterDbPromiseStorage.cs
+++ Server.FasterKV/FasterDbPromiseStorage.cs
@@ -1,11 +1,8 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hearty.Server.FasterKV;
 
@@ -13,14 +10,42 @@ namespace Hearty.Server.FasterKV;
 // Disabled until this library becomes less experimental
 #pragma warning disable CS1591
 
-public class FasterDbPromiseStorage : PromiseStorage, IDisposable
+public sealed class FasterDbPromiseStorage : PromiseStorage, IDisposable
 {
+    /// <summary>
+    /// Promises represented in serialized form in the Faster KV database.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// For performance reasons, <see cref="Promise" /> does not have a finalizer.
+    /// So it is not possible to serialize a promise object only when it is about
+    /// to be collected as garbage; the serialized form must be present in
+    /// the database at the same time as the live object in <see cref="_objects" />.
+    /// However, for persistence and disaster recovery, that is going to
+    /// be required anyway.
+    /// </para>
+    /// </remarks>
     private readonly FasterDbDictionary<PromiseId, ReadOnlyMemory<byte>> _db;
 
+    /// <summary>
+    /// Promises that may have a current live representation as .NET objects.
+    /// </summary>
+    /// <remarks>
+    /// Promise objects that become garbage will have their 
+    /// <see cref="GCHandle" /> set to null.  The null entry will
+    /// get cleaned up periodically or the next time it is
+    /// accessed.
+    /// </remarks>
     private readonly ConcurrentDictionary<PromiseId, GCHandle> _objects = new();
 
-    public FasterDbPromiseStorage()
+    /// <summary>
+    /// Needed to re-materialize promise objects from <see cref="_db" />.
+    /// </summary>
+    private readonly PromiseDataSchemas _schemas;
+
+    public FasterDbPromiseStorage(PromiseDataSchemas schemas)
     {
+        _schemas = schemas;
         _db = new(new FasterDbFileOptions
         {
             Path = string.Empty,
@@ -29,12 +54,29 @@ public class FasterDbPromiseStorage : PromiseStorage, IDisposable
         });
     }
 
+    /// <summary>
+    /// Limit on the number of bytes that serializing a promise results in.
+    /// </summary>
+    private const int MaxSerializationLength = (1 << 24);
+
     /// <inheritdoc />
     public override Promise CreatePromise(PromiseData? input, PromiseData? output = null)
     {
         var promise = CreatePromiseObject(input, output);
 
-        var gcHandle = GCHandle.Alloc(promise);
+        bool canSerialize = promise.TryGetSerializationHeader(out var header) &&
+                            header.TotalLength <= MaxSerializationLength;
+
+        if (canSerialize)
+        {
+            // FIXME: have FASTER KV write to database page directly to avoid copying bytes
+            byte[] bytes = new byte[header.TotalLength];
+            promise.Serialize(header, bytes);
+            _db.Add(promise.Id, new ReadOnlyMemory<byte>(bytes));
+        }
+
+        var gcHandle = GCHandle.Alloc(promise, canSerialize ? GCHandleType.Weak
+                                                            : GCHandleType.Normal);
         try
         {
             if (!_objects.TryAdd(promise.Id, gcHandle))
@@ -71,16 +113,18 @@ public class FasterDbPromiseStorage : PromiseStorage, IDisposable
         // Otherwise, try getting from the database.
         // If it exists, de-serialize the data and then register
         // the live .NET object.
+        //
+        // FIXME: Also try to avoid double copying here
         if (_db.TryGetValue(id, out var data))
         {
-            var promise = DeserializePromise(data);
-            gcHandle = GCHandle.Alloc(promise);
+            var promise = Promise.Deserialize(_schemas, data);
+            gcHandle = GCHandle.Alloc(promise, GCHandleType.Weak);
 
             Promise? promiseLast = null;
             try
             {
-                // Retry loop for rare occurrence another thread tries to
-                // update, and also its Promise object expires soon after.
+                // Retry loop for the rare occurrence when another thread tries
+                // to update, and also its Promise object expires soon after.
                 do
                 {
                     var gcHandleLast = _objects.AddOrUpdate(
@@ -103,12 +147,6 @@ public class FasterDbPromiseStorage : PromiseStorage, IDisposable
         }
 
         return null;
-    }
-
-    private Promise DeserializePromise(ReadOnlyMemory<byte> data)
-    {
-        // FIXME
-        return null!;
     }
 
     public override void SchedulePromiseExpiry(Promise promise, DateTime expiry)
