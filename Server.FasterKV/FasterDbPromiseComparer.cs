3a1b7f960a14af3467be30fe630658257c6ed70f
Steve Cheng <steve.ckp@gmail.com>
2022-03-28 16:49:16 -0400

    Implement comparer for PromiseID keys for FASTER KV databases

---
+++ Server.FasterKV/FasterDbPromiseComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FASTER.core;
+
+namespace Hearty.Server.FasterKV;
+
+/// <summary>
+/// Comparer to use <see cref="PromiseId" /> as a key in a FASTER KV
+/// database efficiently.
+/// </summary>
+public sealed class FasterDbPromiseComparer : IFasterEqualityComparer<PromiseId>
+{
+    /// <inheritdoc cref="IFasterEqualityComparer{T}.Equals" />
+    public bool Equals(ref PromiseId k1, ref PromiseId k2)
+        => k1 == k2;
+
+    /// <inheritdoc cref="IFasterEqualityComparer{T}.GetHashCode64" />
+    public long GetHashCode64(ref PromiseId k) => (long)k.RawInteger;
+}
