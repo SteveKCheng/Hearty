358e65f4142d56a1affc6970836830df6d0ee193
Steve Cheng <steve.ckp@gmail.com>
2022-03-28 16:49:54 -0400

    Add unit test of "hybrid log" (swapping to files) for FasterDbDictionary

--- Tests/FasterKVTests.cs
+++ Tests/FasterKVTests.cs
@@ -68,4 +68,46 @@ public class FasterKVTests
         Assert.Equal(storedValue, retrievedValue);
         Assert.InRange(state.value, 0.0, 1.0);
     }
+
+    [Fact]
+    public void AddMany()
+    {
+        uint serviceId = 0;
+
+        int count = 1 << 16;
+
+        using var db = new FasterDbDictionary<PromiseId, double>(new FasterDbFileOptions
+        {
+            Path = String.Empty,
+            Preallocate = false,
+            DeleteOnDispose = true,
+            HashIndexSize = count,
+
+            PageLog2Size = 16,
+
+            // There are 2^16 items to write but only 2^20 bytes of memory,
+            // while each item takes up more than 16 bytes.  Both key and value
+            // are 8 bytes but there is also the record header.
+            //
+            // This setting thus forces FASTER KV to swap to file-backed storage.
+            // So, operations in FASTER KV can become asynchronous (but
+            // our wrapper waits for them synchronously).
+            MemoryLog2Capacity = 20
+        }, new FasterDbPromiseComparer());
+
+        for (int i = 0; i < count; ++i)
+        {
+            var key = new PromiseId(serviceId, (uint)(i + 1));
+            var value = (double)(i + 1);
+            db.Add(key, value);
+        }
+
+        // Re-read numbers some of which should come from file-backed storage
+        for (int i = 0; i < count; ++i)
+        {
+            var key = new PromiseId(serviceId, (uint)(i + 1));
+            Assert.True(db.TryGetValue(key, out var value));
+            Assert.Equal((double)(i + 1), value);
+        }
+    }
 }
