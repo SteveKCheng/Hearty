231032baf8f375e5de1095b5f4bb304b452150f0
Steve Cheng <steve.ckp@gmail.com>
2022-04-03 13:37:29 -0400

    Support LINQ expressions in PropertyColumn for Blazor grid control
    The original code (which had been copied from a Microsoft demo)
    supported LINQ expressions already.  For efficiency and simplicity
    we had removed that support.
    
    Now we add it back, although the feature remains optional where
    it was mandatory in the original code.  Items sources for the
    grid control which do not come from a database, i.e. does
    not implement IQueryable<TGridItem>, can continue to supply plain
    functions for the property projection.

--- Server.WebUi/PropertyColumn.cs
+++ Server.WebUi/PropertyColumn.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using System.Linq;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Hearty.Server.WebUi;
 
@@ -16,14 +17,52 @@ namespace Hearty.Server.WebUi;
 /// </typeparam>
 public sealed class PropertyColumn<TGridItem, TProp> : ColumnDefinition<TGridItem>
 {
-    private object? _cachedProperty;
+    private Func<TGridItem, TProp>? _property;
+    private Expression<Func<TGridItem, TProp>>? _expression;
+    private bool _propertyNeedsRendering;
 
     /// <summary>
     /// Projects an item (row) into a value to display in this column.
     /// </summary>
-    [Parameter, EditorRequired] 
-    public Func<TGridItem, TProp> Property { get; set; } = default!;
-    
+    /// <remarks>
+    /// This function is called for rendering the item after it
+    /// has been received (possibly from a database).
+    /// If this member is null but <see cref="Expression" />
+    /// is not, then the latter is compiled into a function.
+    /// Specify both properties to avoid run-time compilation
+    /// of LINQ expressions.
+    /// </remarks>
+    [Parameter]
+    public Func<TGridItem, TProp>? Property
+    {
+        get => _property;
+        set
+        {
+            _propertyNeedsRendering = true;
+            _property = value;
+        }
+    }
+
+    /// <summary>
+    /// Projects an item (row) into a value to display in this column,
+    /// as a LINQ expression.
+    /// </summary>
+    /// <remarks>
+    /// This LINQ expression can be incorporated as part of a
+    /// query, to let the LINQ provider (database) do filtering 
+    /// and sorting.
+    /// </remarks>
+    [Parameter]
+    public Expression<Func<TGridItem, TProp>>? Expression
+    {
+        get => _expression;
+        set
+        {
+            _propertyNeedsRendering = true;
+            _expression = value;
+        }
+    }
+
     /// <summary>
     /// The format specification to convert <typeparamref name="TProp" /> 
     /// for displaying in a grid cell.
@@ -41,43 +80,55 @@ public sealed class PropertyColumn<TGridItem, TProp> : ColumnDefinition<TGridIte
     /// <inheritdoc />
     protected override void OnParametersSet()
     {
-        if (!object.ReferenceEquals(_cachedProperty, Property))
+        if (!_propertyNeedsRendering)
+            return;
+
+        if (_property is null)
         {
-            _cachedProperty = Property;
-
-            Func<TGridItem, string> cellTextFunc;
-            if (!string.IsNullOrEmpty(Format))
+            if (_expression is null)
             {
-                Type propertyType = Nullable.GetUnderlyingType(typeof(TProp)) ?? typeof(TProp);
-
-                if (!typeof(IFormattable).IsAssignableFrom(propertyType))
-                {
-                    throw new InvalidOperationException(
-                        $"A '{nameof(Format)}' parameter was supplied, but the type '{typeof(TProp)}' does not implement '{typeof(IFormattable)}'.");
-                }
-
-                cellTextFunc = item => ((IFormattable?)Property.Invoke(item))?.ToString(Format, null) ?? string.Empty;
-            }
-            else
-            {
-                cellTextFunc = item => Property.Invoke(item)?.ToString() ?? string.Empty;
+                _propertyNeedsRendering = false;
+                CellContent = EmptyChildContent;
+                return;
             }
 
-            if (OnCellClicked.HasDelegate)
-            {
-                CellContent = item => builder =>
-                {
-                    builder.OpenElement(0, "button");
-                    builder.AddAttribute(1, "onclick", () => OnCellClicked.InvokeAsync(item));
-                    builder.AddContent(2, cellTextFunc(item));
-                    builder.CloseElement();
-                };
-            }
-            else
-            {
-                CellContent = item => builder => builder.AddContent(0, cellTextFunc(item));
-            }
+            _property = _expression.Compile();
         }
+
+        Func<TGridItem, string> cellTextFunc;
+        if (!string.IsNullOrEmpty(Format))
+        {
+            Type propertyType = Nullable.GetUnderlyingType(typeof(TProp)) ?? typeof(TProp);
+
+            if (!typeof(IFormattable).IsAssignableFrom(propertyType))
+            {
+                throw new InvalidOperationException(
+                    $"A '{nameof(Format)}' parameter was supplied, but the type '{typeof(TProp)}' does not implement '{typeof(IFormattable)}'.");
+            }
+
+            cellTextFunc = item => ((IFormattable?)_property!.Invoke(item))?.ToString(Format, null) ?? string.Empty;
+        }
+        else
+        {
+            cellTextFunc = item => _property!.Invoke(item)?.ToString() ?? string.Empty;
+        }
+
+        if (OnCellClicked.HasDelegate)
+        {
+            CellContent = item => builder =>
+            {
+                builder.OpenElement(0, "button");
+                builder.AddAttribute(1, "onclick", () => OnCellClicked.InvokeAsync(item));
+                builder.AddContent(2, cellTextFunc(item));
+                builder.CloseElement();
+            };
+        }
+        else
+        {
+            CellContent = item => builder => builder.AddContent(0, cellTextFunc(item));
+        }
+
+        _propertyNeedsRendering = false;
     }
 
     /// <inheritdoc />
@@ -86,7 +137,19 @@ public sealed class PropertyColumn<TGridItem, TProp> : ColumnDefinition<TGridIte
     /// <inheritdoc />
     public override IEnumerable<TGridItem> GetSortedItems(IEnumerable<TGridItem> source, bool ascending)
     {
-        return ascending ? source.OrderBy(Property)
-                         : source.OrderByDescending(Property);
+        if (_expression is { }pe && source is IQueryable<TGridItem> queryable)
+        {
+            return ascending ? queryable.OrderBy(pe)
+                             : queryable.OrderByDescending(pe);
+        }
+        else if (_property is { }p)
+        {
+            return ascending ? source.OrderBy(p)
+                             : source.OrderByDescending(p);
+        }
+        else
+        {
+            return source;
+        }
     }
 }
