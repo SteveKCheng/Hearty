de8b4df2b51283d3f9750780b599ad7dec01c197
Steve Cheng <steve.ckp@gmail.com>
2022-06-10 21:21:38 -0400

    Add option to suppress "disconnect" button in Blazor WorkersView

---
+++ Server.WebUi/Pages/WorkersView.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearty.Server.WebUi.Pages;
+
+public partial class WorkersView
+{
+    /// <inheritdoc />
+    protected override void OnInitialized()
+        => StartRefreshing(TimeoutBucket.After5Seconds);
+
+    /// <summary>
+    /// If true, allow workers to be disconnected through this UI control.
+    /// </summary>
+    /// <remarks>
+    /// Clients of this UI control may want to set this parameter 
+    /// according to whether the logged in user is authorized to
+    /// disconnect workers.
+    /// </remarks>
+    [Parameter]
+    public bool AllowDisconnect { get; set; } = true;
+}
